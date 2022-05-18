ce2d9b949edb9bfb69971cfecf815a80da2dbfca
bpmurray77 <bpmurray77@gmail.com>
2022-05-18 23:51:02 +0000

    progress

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#excuse").innerHTML = randomNumber;
+  console.log("test" + randomNumber);
 };
+
 let generateExcuse = () => {
   let subject = {};
   let action = {};
